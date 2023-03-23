12c896ae3532a0351ce53b3bbc71f09aa70e8c40
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-03-23 15:42:17 -0300

    recurso(ClienteController): Criacao do Controller

---
+++ app/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cliente;
+use App\Http\Requests\ClienteRequest;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ClienteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ClienteRequest $request, Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ClienteRequest $cliente)
+    {
+        //
+    }
+}
