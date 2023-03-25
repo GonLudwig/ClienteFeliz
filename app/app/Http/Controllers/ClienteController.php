2c85a98a7c4dd09ae2babbe1898c41e5087b99a9
GlaucoStarling <glaucox.x@gmail.com>
2023-03-24 22:18:51 -0300

    recurso(ClienteController): Implementacao do CrudService

--- app/app/Http/Controllers/ClienteController.php
+++ app/app/Http/Controllers/ClienteController.php
@@ -4,53 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Cliente;
 use App\Http\Requests\ClienteRequest;
+use App\Service\CrudService;
 
 class ClienteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $data['data'] = Cliente::all();
-        
-        return response()->json($data);
+        $response = CrudService::buscar(new Cliente());
+        return response()->json($response);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(ClienteRequest $request)
     {
-        $cliente = Cliente::create($request->all());
-        return response()->json($cliente);
+        $response = CrudService::criar(new Cliente(), $request);
+        return response()->json($response);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    public function update(ClienteRequest $request, Cliente $cliente)
+    public function update(ClienteRequest $request, String $cliente)
     {
-        return response()->json($request->all());
+        $response = CrudService::atualizar(new Cliente(), $request, $cliente);
+        return response()->json($response);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Cliente $cliente)
+    public function destroy(String $cliente)
     {
-        return response()->json($cliente->delete());
+        $response = CrudService::deletar(new Cliente(), $cliente);
+        return response()->json($response);
     }
 }
